05254bd50bd4012885cad536a797fcfee9a2b248
Steven Janssens <steven.janssens@utoronto.ca>
2024-07-30 00:03:52 -0600

    golang options

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,16 @@ vim.api.nvim_create_autocmd("TextYankPost", {
         })
     end,
 })
+
+-- Go
+vim.api.nvim_create_autocmd("BufEnter", {
+    pattern = { "*.go" },
+    callback = function()
+        vim.opt_local.expandtab = false
+        vim.opt_local.copyindent = true
+        vim.opt_local.preserveindent = true
+        vim.opt_local.softtabstop = 0
+        vim.opt_local.shiftwidth = 4
+        vim.opt_local.tabstop = 4
+    end,
+})
