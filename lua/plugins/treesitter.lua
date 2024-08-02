8be89011a2decfd8633adae18fdd7b947b00c3fb
Steven Janssens <steven.janssens@utoronto.ca>
2024-08-02 03:41:14 -0600

    disable nvim-treesitter indent module for python

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -55,7 +55,8 @@ return {
             },
 
             indent = {
-                enable = true
+                enable = true,
+                disable = { "python" },
             },
         })
     end,
