d7e93eb6452d0b8830bc6345c77a8c6fbd6042f0
Steven Janssens <steven.janssens@utoronto.ca>
2024-08-03 02:21:47 -0600

    added keymap to fuzzy find keymaps

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -10,6 +10,7 @@ return {
         vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "Fuzzy find grep" })
         vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "Fuzzy find help" })
         vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "Resume fuzzy find" })
+        vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "Fuzzy find keymaps" })
         vim.keymap.set("n", "<C-p>", builtin.git_files, {desc = "Fuzzy find git (project) files" })
         vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "Fuzzy find buffers" })
         vim.keymap.set("n", "<leader>/", builtin.current_buffer_fuzzy_find, { desc = "Fuzzy find current buffer" })
