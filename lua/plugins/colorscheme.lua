cba99a6ba717a125e299be42df77a451489d91e4
Steven Janssens <steven.janssens@utoronto.ca>
2024-08-05 03:31:34 -0600

    trying gruvbox-material

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,10 +1,11 @@
-return {
-    "NLKNguyen/papercolor-theme",
-    priority = 1000,
-    init = function()
-        vim.cmd.colorscheme("papercolor")
-    end,
-}
+-- PaperColor doesn't support semantic highlighting
+-- return {
+--     "NLKNguyen/papercolor-theme",
+--     priority = 1000,
+--     init = function()
+--         vim.cmd.colorscheme("papercolor")
+--     end,
+-- }
 
 -- return {
 --     {
@@ -66,3 +67,19 @@ return {
 --         end,
 --     }
 -- }
+
+return {
+    {
+        'sainnhe/gruvbox-material',
+        lazy = false,
+        priority = 1000,
+        config = function()
+            -- Optionally configure and load the colorscheme
+            -- directly inside the plugin declaration.
+            vim.g.gruvbox_material_enable_italic = false
+            vim.g.gruvbox_material_background = "hard"
+            vim.g.gruvbox_material_better_performance = 1
+            vim.cmd.colorscheme("gruvbox-material")
+        end
+    }
+}
