c3f927a218a4654cdac61d1e477ca74fd033b214
Steven Janssens <steven.janssens@utoronto.ca>
2024-08-05 04:09:00 -0600

    trying tokyonight

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -70,17 +70,49 @@
 --     }
 -- }
 
+-- return {
+--     {
+--         'sainnhe/gruvbox-material',
+--         lazy = false,
+--         priority = 1000,
+--         config = function()
+--             vim.g.gruvbox_material_background = "hard"
+--             vim.g.gruvbox_material_better_performance = 1
+--             vim.g.gruvbox_material_enable_italic = true
+--             vim.g.gruvbox_material_disable_italic_comment = true
+--             vim.cmd.colorscheme("gruvbox-material")
+--         end
+--     }
+-- }
+
 return {
     {
-        'sainnhe/gruvbox-material',
+        "folke/tokyonight.nvim",
         lazy = false,
         priority = 1000,
-        config = function()
-            vim.g.gruvbox_material_background = "hard"
-            vim.g.gruvbox_material_better_performance = 1
-            vim.g.gruvbox_material_enable_italic = true
-            vim.g.gruvbox_material_disable_italic_comment = true
-            vim.cmd.colorscheme("gruvbox-material")
+        opts = {
+            style = "day", -- The theme comes in three styles, `storm`, a darker variant `night` and `day`
+            light_style = "day", -- The theme is used when the background is set to light
+            transparent = false, -- Enable this to disable setting the background color
+            terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
+            styles = {
+                -- Style to be applied to different syntax groups
+                -- Value is any valid attr-list value for `:help nvim_set_hl`
+                comments = { italic = false },
+                keywords = { italic = false },
+                functions = {},
+                variables = {},
+                -- Background styles. Can be "dark", "transparent" or "normal"
+                sidebars = "dark", -- style for sidebars, see below
+                floats = "dark", -- style for floating windows
+            },
+            day_brightness = 0.25, -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
+            dim_inactive = false, -- dims inactive windows
+            lualine_bold = false, -- When `true`, section headers in the lualine theme will be bold
+            cache = true, -- When set to true, the theme will be cached for better performance
+        },
+        init = function()
+            vim.cmd.colorscheme("tokyonight")
         end
     }
 }
