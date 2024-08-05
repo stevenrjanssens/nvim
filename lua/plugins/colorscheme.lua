b2db3477df788e17bf2fa75ae72ed3e61e83250c
Steven Janssens <steven.janssens@utoronto.ca>
2024-08-05 03:34:07 -0600

    disable gruvbox-material italic comments

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -74,11 +74,10 @@ return {
         lazy = false,
         priority = 1000,
         config = function()
-            -- Optionally configure and load the colorscheme
-            -- directly inside the plugin declaration.
-            vim.g.gruvbox_material_enable_italic = false
             vim.g.gruvbox_material_background = "hard"
             vim.g.gruvbox_material_better_performance = 1
+            vim.g.gruvbox_material_enable_italic = true
+            vim.g.gruvbox_material_disable_italic_comment = true
             vim.cmd.colorscheme("gruvbox-material")
         end
     }
