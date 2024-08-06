a0330690bef59c446ffd1c43519de779ae8e92e3
Steven Janssens <steven.janssens@utoronto.ca>
2024-08-05 18:00:45 -0600

    moved lazy init to init.lua

--- init.lua
+++ init.lua
@@ -2,5 +2,21 @@ require("config/options")
 require("config/mappings")
 require("config/autocmds")
 
--- Load last so <leader> and other settings all set
-require("config/lazy")
+-- install lazy.nvim, do last so <leader> and other settings all set
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+   vim.fn.system({
+      "git",
+      "clone",
+      "--filter=blob:none",
+      "https://github.com/folke/lazy.nvim.git",
+      "--branch=stable", -- latest stable release
+      lazypath,
+   })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- load plugins
+require("lazy").setup({
+    spec = "plugins"
+})
