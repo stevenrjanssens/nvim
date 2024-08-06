f3548c9dc972f7783d19ac46b149a5db5e073ce4
Steven Janssens <steven.janssens@utoronto.ca>
2024-08-05 19:44:47 -0600

    set lualine theme, removed separators, removed fileformat

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -6,9 +6,11 @@ return {
             require('lualine').setup({
                 options = {
                     icons_enabled = true,
-                    theme = 'auto',
-                    component_separators = { left = '', right = ''},
-                    section_separators = { left = '', right = ''},
+                    theme = 'catppuccin',
+                    -- component_separators = { left = '', right = ''},
+                    -- section_separators = { left = '', right = ''},
+                    component_separators = { left = '', right = ''},
+                    section_separators = { left = '', right = ''},
                     disabled_filetypes = {
                         statusline = {},
                         winbar = {},
@@ -26,7 +28,8 @@ return {
                     lualine_a = {'mode'},
                     lualine_b = {'branch', 'diff', 'diagnostics'},
                     lualine_c = {'filename'},
-                    lualine_x = {'encoding', 'fileformat', 'filetype'},
+                    -- lualine_x = {'encoding', 'fileformat', 'filetype'},
+                    lualine_x = {'encoding', 'filetype'},
                     lualine_y = {'progress'},
                     lualine_z = {'location'}
                 },
