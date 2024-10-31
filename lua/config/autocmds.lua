57de6ad18c98b3f44561687038c9c57f1597568d
Steven Janssens <steven.janssens@utoronto.ca>
2024-10-31 04:57:19 -0600

    moved filetype settings to after/ftplugin

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -18,27 +18,6 @@ autocmd("BufEnter", {
   desc = "Disable New Line Comment",
 })
 
--- Go
-autocmd("BufEnter", {
-    pattern = { "*.go" },
-    callback = function()
-        vim.opt_local.expandtab = false
-        vim.opt_local.copyindent = true
-        vim.opt_local.preserveindent = true
-        vim.opt_local.softtabstop = 0
-        vim.opt_local.shiftwidth = 4
-        vim.opt_local.tabstop = 4
-    end,
-})
-
--- LaTeX
-autocmd("BufEnter", {
-    pattern = { "*.tex" },
-    callback = function()
-        vim.opt_local.textwidth = 78
-    end,
-})
-
 -- Create the lsp keymaps only when a language server is active
 vim.api.nvim_create_autocmd('LspAttach', {
   desc = 'LSP actions',
