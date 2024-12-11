e617b262aad66ca20b9d76b58ded1067d9cd4a51
Steven Janssens <steven.janssens@utoronto.ca>
2024-12-11 03:28:05 -0700

    added telescope-fzf-native extension

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,9 +1,13 @@
 return {
     "nvim-telescope/telescope.nvim",
     branch = "0.1.x",
-    dependencies = { "nvim-lua/plenary.nvim" },
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
+    },
     config = function()
         require("telescope").setup({})
+        require("telescope").load_extension('fzf')
 
         local builtin = require("telescope.builtin")
         vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "Fuzzy find all files" })
