5d36b8c9be62f6b756d751bf27f11d4ca3d42318
Steven Janssens <steven.janssens@utoronto.ca>
2024-12-16 20:13:40 -0700

    changed quickfix navigation mappings

--- lua/config/mappings.lua
+++ lua/config/mappings.lua
@@ -8,7 +8,7 @@ vim.keymap.set({"n"}, "<leader>Y", [["+Y]], { desc = "Yank line(s) into system c
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<cr>")
 
 -- Quickfix navigation
-vim.keymap.set("n", "<C-k>", "<cmd>cnext<cr>zz")
-vim.keymap.set("n", "<C-j>", "<cmd>cprev<cr>zz")
-vim.keymap.set("n", "<leader>k", "<cmd>lnext<cr>zz")
-vim.keymap.set("n", "<leader>j", "<cmd>lprev<cr>zz")
+vim.keymap.set("n", "<C-j>", "<cmd>cnext<cr>zz")
+vim.keymap.set("n", "<C-k>", "<cmd>cprev<cr>zz")
+vim.keymap.set("n", "<leader>j", "<cmd>lnext<cr>zz")
+vim.keymap.set("n", "<leader>k", "<cmd>lprev<cr>zz")
