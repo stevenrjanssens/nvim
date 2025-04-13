4801ca6a2659ddc9b84ccf876cfc7d6af375d9e8
Steven Janssens <steven.janssens@dragonfly1000.com>
2025-04-13 05:16:08 -0600

    configure ruff plugin for pylsp

--- lsp/pylsp.lua
+++ lsp/pylsp.lua
@@ -1,5 +1,23 @@
+--[[
+    Prerequisites:
+    - ruff
+    - python-lsp-server and python-lsp-ruff need to be installed in the same env
+    ]]--
 return {
     cmd = { "pylsp" },
     root_markers = { "pyproject.toml", "requirements.txt", "setup.py", ".git"},
-    filetypes = { "python" }
+    filetypes = { "python" },
+    settings = {
+        pylsp = {
+            plugins = {
+                ruff = {
+                    enabled = true,
+                    formatEnabled = true,
+                    executable = "ruff",
+                    extendSelect = { "I" },  -- enable import sorting
+                    perFileIgnores = { ["__init__.py"] = "CPY001" }
+                }
+            }
+        }
+    }
 }
